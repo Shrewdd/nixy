e718afe634bc9c67e3d84cd14153ccceaa0e5001
qkbp <qkbpp@protonmail.com>
2025-08-15 17:45:52 +0200

    home-manager(zen-browser): forward inputs and re-export upstream module

--- home-manager/zen-browser/default.nix
+++ home-manager/zen-browser/default.nix
@@ -1,5 +1,5 @@
-{ ... }:
+{ pkgs, lib, inputs, ... }:
 
 # Forward all arguments to the upstream zen-browser module so this file is a
 # valid home-manager module that simply re-exports the upstream module.
-(import inputs.zen-browser.homeModules.beta) /* the upstream module */
+inputs.zen-browser.homeModules.beta
