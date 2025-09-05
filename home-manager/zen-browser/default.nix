11db11322bc7e888ab5b057cc4bee64fc4a86a8c
qkbp <qkbpp@protonmail.com>
2025-09-05 10:56:08 +0200

    new zen browser theme, new policy and added comments for readability

--- home-manager/zen-browser/default.nix
+++ home-manager/zen-browser/default.nix
@@ -6,7 +6,13 @@
 		enable = true;
 		policies = {
 			Preferences = {
+				# Warns the user when attempting to close multiple tabs
 				"browser.tabs.warnOnClose" = {
+					Value = true;
+					Status = "locked";
+				};
+				# Enables custom stylesheets (userChrome.css and userContent.css) for UI customization
+				"toolkit.legacyUserProfileCustomizations.stylesheets" = {
 					Value = true;
 					Status = "locked";
 				};
