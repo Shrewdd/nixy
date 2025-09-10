fc600c62f0888788bf84134350f29a3b159b991f
qkbp <qkbpp@protonmail.com>
2025-09-10 17:40:58 +0200

    feat: enhance Spotify with Catppuccin theme and compact UI

--- home-manager/spicetify-nix/default.nix
+++ home-manager/spicetify-nix/default.nix
@@ -1,12 +1,25 @@
 { pkgs, lib, inputs, ... }:
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+  theme = import ../../themes/catppuccin-macchiato.nix;
 in
 {
   imports = [ inputs.spicetify-nix.homeManagerModules.spicetify ];
 
   programs.spicetify = {
     enable = true;
-    enabledExtensions = with spicePkgs.extensions; [ adblockify ];
+
+    # Catppuccin theme with Macchiato colors
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "macchiato";
+
+    enabledExtensions = with spicePkgs.extensions; [
+      adblockify
+      fullAppDisplay
+      shuffle
+      copyToClipboard
+      powerBar
+      volumePercentage
+    ];
   };
 }
