194ce3b54a4768b02196fc3e4135f2b9ddc456f2
qkbp <qkbpp@protonmail.com>
2025-11-06 18:14:53 +0100

    feat(niri): enable Avahi service for mDNS resolution and open firewall

--- hosts/monsoon/modules/services.nix
+++ hosts/monsoon/modules/services.nix
@@ -10,6 +10,12 @@
     enable = true;
   };
 
+  # Enable Avahi for mDNS resolution (.local names)
+  services.avahi = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # Enable udev for GNOME DE
   services.udev.packages = with pkgs; [ gnomeExtensions.appindicator ];
 
