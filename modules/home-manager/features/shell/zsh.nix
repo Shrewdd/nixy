efa108cc0dd7df90f50d44810022e2c163159354
qkbp <qkbpp@protonmail.com>
2025-11-26 17:22:00 +0100

    feat(zsh): wire fzf keybindings and remove duplicate packages\n\n- Add Ctrl-R (history), Ctrl-T (files), Alt-C (cd) when fzf widgets exist\n- Keep fd/rg for fast search; rely on programs.fzf/zoxide for install\n- No aliases or command overrides

--- modules/home-manager/features/shell/zsh.nix
+++ modules/home-manager/features/shell/zsh.nix
@@ -10,6 +10,11 @@
       enable = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+      history = {
+        size = 10000;
+        save = 10000;
+        share = true;
+      };
       
       shellAliases = {
         ls = "ls --color=auto";
@@ -21,8 +26,29 @@
 
       initContent = ''
         # Add any custom zsh init here
+        # fzf defaults
+        if command -v fzf >/dev/null 2>&1; then
+          export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border --preview-window=down:60%'
+          export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+          # Standard fzf keybindings if widgets are available
+          # Ctrl-T: file picker, Alt-C: cd, Ctrl-R: history search
+          if typeset -f fzf-file-widget >/dev/null; then bindkey '^T' fzf-file-widget; fi
+          if typeset -f fzf-history-widget >/dev/null; then bindkey '^R' fzf-history-widget; fi
+          if typeset -f fzf-cd-widget >/dev/null; then bindkey '^[c' fzf-cd-widget; fi
+        fi
+        # zoxide init
+        eval "$(zoxide init zsh)"
       '';
     };
+
+    # Interactive fuzzy finder + smart cd
+    programs.fzf.enable = true;
+    programs.zoxide.enable = true;
+
+    # Nice CLI tools used by aliases and fzf
+    # fd/ripgrep provide fast search used by fzf defaults.
+    # fzf and zoxide come from programs.* above, no need to duplicate here.
+    home.packages = with pkgs; [ fd ripgrep ];
 
     # Make Bash exec Zsh in interactive shells
     programs.bash = {
