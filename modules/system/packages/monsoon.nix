66fcd332fe05a83f628e9c69173c7ff5f1f0f695
qkbp <qkbpp@protonmail.com>
2025-12-18 14:11:41 +0100

    Remove unnecessary packages from monsoon.nix

--- modules/system/packages/monsoon.nix
+++ modules/system/packages/monsoon.nix
@@ -2,18 +2,11 @@
 {
   environment.systemPackages = with pkgs; [
     vesktop
-    lutris
     sixpair
     appimage-run
     lshw
     speedtest-cli
-    rojo
-    wally
-    selene
-    luau-lsp
-    stylua
   ];
 
   programs.gamemode.enable = true;
-  programs.steam.enable = true;
 }
