ac0866837171c163423819d34f7ea689e303ebb5
qkbp <qkbpp@protonmail.com>
2026-02-01 13:41:12 +0100

    fix(printing): disable avahi service by default

--- modules/system/services/printing.nix
+++ modules/system/services/printing.nix
@@ -5,8 +5,9 @@
     drivers = with pkgs; [ gutenprint hplip ];
   };
 
+  # Need to figure out why avahi fails to start mDNS/DNS-SD Stack
   services.avahi = {
-    enable = lib.mkDefault true;
+    enable = lib.mkDefault false;
     nssmdns4 = true;
     openFirewall = true;
   };
