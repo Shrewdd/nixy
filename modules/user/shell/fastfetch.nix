c11ac6c76c42d72437267ddbbeb2c5c97f334d3c
qkbp <qkbpp@protonmail.com>
2026-02-20 18:23:51 +0100

    chore: update fastfetch logo and enhance display settings

--- modules/user/shell/fastfetch.nix
+++ modules/user/shell/fastfetch.nix
@@ -3,101 +3,130 @@
     enable = true;
     settings = {
       logo = {
-        type = "small";
-        height = 18;
+        type = "kitty-direct";
+        source = "${toString ./../shared/logos/nixos.png}";
+        width = 34;
+        padding = {
+          top = 8;
+          bottom = 8;
+          right = 5;
+          left = 5;
+        };
       };
       display = {
-        separator = " : ";
+        separator = " -> ";
       };
       modules = [
-        {
-          type = "custom";
-          format = "┌──────────────────────────────────────────────────┐";
-        }
-        {
-          type = "os";
-          key = "  󰣇 OS";
-          format = "{2}";
-          keyColor = "red";
-        }
-        {
-          type = "kernel";
-          key = "  󰌽 Kernel";
-          format = "{2}";
-          keyColor = "red";
-        }
-        {
-          type = "packages";
-          key = "  󰏗 Packages";
-          keyColor = "green";
-        }
-        {
-          type = "display";
-          key = "  󰍹 Display";
-          format = "{1}x{2} @ {3}Hz [{7}]";
-          keyColor = "green";
-        }
-        {
-          type = "terminal";
-          key = "  󰞳 Terminal";
-          keyColor = "yellow";
-        }
-        {
-          type = "wm";
-          key = "  󱗃 WM";
-          format = "{2}";
-          keyColor = "yellow";
-        }
-        {
-          type = "custom";
-          format = "└──────────────────────────────────────────────────┘";
-        }
         "break"
         {
-          type = "title";
-          key = "  ";
-          format = "{6} {7} {8}";
+          type = "custom";
+          format = "╭────────────────────── Hardware ──────────────────────╮";
+          outputColor = "red";
         }
         {
-          type = "custom";
-          format = "┌──────────────────────────────────────────────────┐";
+          type = "title";
+          key = "  PC";
+          keyColor = "green";
+          format = "{user-name-colored} on {host-name-colored}";
         }
         {
           type = "cpu";
-          format = "{1} @ {7}";
-          key = "  󰍛 CPU";
-          keyColor = "blue";
+          key = "│ ├󰍛 CPU";
+          format = "{name} {freq-max}";
+          keyColor = "green";
         }
         {
           type = "gpu";
-          format = "{1} {2}";
-          key = "  󰊴 GPU";
-          keyColor = "blue";
-        }
-        {
-          type = "memory";
-          key = "  󰍛 Memory";
-          keyColor = "magenta";
+          key = "│ ├󰍛 GPU";
+          keyColor = "green";
+          format = "{vendor} {name}";
         }
         {
           type = "disk";
-          key = "  󱦟 OS Age";
-          folders = "/";
-          keyColor = "red";
-          format = "{days} days";
+          key = "│ ├󱛟 Disk";
+          keyColor = "green";
         }
         {
-          type = "uptime";
-          key = "  󱫐 Uptime";
-          keyColor = "red";
+          type = "memory";
+          key = "└ └󰍛 Memory";
+          keyColor = "green";
         }
         {
           type = "custom";
-          format = "└──────────────────────────────────────────────────┘";
+          format = "╰──────────────────────────────────────────────────────╯";
+          outputColor = "red";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "╭────────────────────── Software ──────────────────────╮";
+          outputColor = "red";
+        }
+        {
+          type = "os";
+          key = "  OS";
+          keyColor = "yellow";
+          format = "NixOS {version-id}";
+        }
+        {
+          type = "kernel";
+          key = "│ ├ Kernel";
+          keyColor = "yellow";
+        }
+        {
+          type = "shell";
+          key = "│ ├ Shell";
+          keyColor = "yellow";
+          format = "{1}";
+        }
+        {
+          type = "packages";
+          key = "│ ├󰏖 Packages";
+          keyColor = "yellow";
+        }
+        {
+          type = "uptime";
+          key = "└ └ Uptime";
+          keyColor = "yellow";
+        }
+        {
+          type = "custom";
+          format = "╰──────────────────────────────────────────────────────╯";
+          outputColor = "red";
+        }
+        "break"
+        {
+          type = "de";
+          key = "  DE";
+          keyColor = "blue";
+          format = "{1}";
+        }
+        {
+          type = "wm";
+          key = "│ ├ Compositor";
+          keyColor = "blue";
+          format = "{1}";
+        }
+        {
+          type = "lm";
+          key = "│ ├ Login";
+          keyColor = "blue";
+          format = "{1}";
+        }
+        {
+          type = "terminal";
+          key = "│ ├ Terminal";
+          keyColor = "blue";
+          format = "{1}";
+        }
+        {
+          type = "icons";
+          key = "│ ├󰄛 Icons";
+          keyColor = "blue";
         }
         {
           type = "colors";
-          paddingLeft = 2;
+          paddingLeft = 20;
           symbol = "circle";
         }
         "break"
