85dd6fcac744448e5d841f0cd4c0253112733cf0
qkbp <qkbpp@protonmail.com>
2026-02-20 12:00:36 +0100

    chore: remove unused nvidia prime config
    Since monsoon's display is hard-wired to NVIDIA GPU via HDMI,
    Prime GPU switching is unused. Remove for cleaner configuration.

--- hosts/monsoon/configuration.nix
+++ hosts/monsoon/configuration.nix
@@ -20,11 +20,6 @@
     powerManagement.finegrained = false;
     package = config.boot.kernelPackages.nvidiaPackages.production;
     nvidiaSettings = true;
-    prime = {
-      sync.enable = true;
-      nvidiaBusId = "PCI:1:0:0";
-      intelBusId = "PCI:0:2:0";
-    };
   };
 
   boot.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
