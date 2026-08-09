d351866c095237cbef2983d3324a3ca434f534c0
qkbp <qkbpp@protonmail.com>
2026-08-09 10:47:51 +0200

    feat: add two wallpapers for rose-pine

--- modules/nixos/stylix/themes.nix
+++ modules/nixos/stylix/themes.nix
@@ -8,6 +8,10 @@
         name = "leafy-dawn.jpg";
         path = ./wallpapers/leafy-dawn.jpg;
       }
+      {
+        name = "mountains-dawn.jpg";
+        path = ./wallpapers/mountains-dawn.jpg;
+      }
     ];
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-dawn.yaml";
     polarity = "light";
@@ -19,6 +23,10 @@
       {
         name = "leafy-moon.jpg";
         path = ./wallpapers/leafy-moon.jpg;
+      }
+      {
+        name = "mountains-moon.jpg";
+        path = ./wallpapers/mountains-moon.jpg;
       }
     ];
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
