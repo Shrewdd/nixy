005a2f1a84349e093954f5fcc0c6e3db1773c5c3
qkbp <qkbpp@protonmail.com>
2026-08-21 14:30:59 +0200

    chore: remove unused plasma manager and duplicate hm revisions

--- flake.nix
+++ flake.nix
@@ -10,13 +10,10 @@
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
     spotify = {
       url = "github:Gerg-L/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     caelestia-shell = {
